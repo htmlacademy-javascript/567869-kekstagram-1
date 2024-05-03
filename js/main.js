7b16a25c32d28f7b77f996f972a589c34e8e59cc
Danila <danila31rusk@ya.ru>
2024-05-04 00:31:51 +0300

    Исправил ошибки по комментариям наставника и упростил написание ассинхроного кода с помощью async/await

--- js/main.js
+++ js/main.js
@@ -3,14 +3,11 @@ import { renderGallery } from './gallery.js';
 import { getData } from './api.js';
 import { showAlert } from './util.js';
 
-getData()
-  .then((pictures) => {
-    renderGallery(pictures);
-  })
-  .catch(
-    (err) => {
-      showAlert(err.message);
-    }
-  );
+try {
+  const pictures = await getData();
+  renderGallery(pictures);
+} catch (err) {
+  showAlert(err.message);
+}
 
 imageUploadInit();
